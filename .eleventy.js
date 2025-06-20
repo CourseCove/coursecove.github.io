da48c3038446a9f8f84f4d246f92cb94c2ba6e2a
coursecove <fastitjobs2k@gmail.com>
2025-06-21 02:08:44 +0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,15 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("css"); // if you have CSS folder
+  // Treat markdown files in _posts as collection "post"
+  eleventyConfig.addCollection("post", function(collection) {
+    return collection.getFilteredByGlob("_posts/*.md");
+  });
+
   return {
     dir: {
-      input: ".",
-      includes: "_includes",
-      output: "_site"
+      input: ".",            // root is input
+      includes: "_includes", // optional (use if you plan to use layouts)
+      data: "_data",         // optional
+      output: "CourseCove"   // final HTML output goes here
     }
   };
 };
